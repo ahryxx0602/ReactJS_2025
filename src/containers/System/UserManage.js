41d37842e8b3d23c0fc83032eae2853c231bd5d5
Ahryxx <phvanthanh06@gmail.com>
2025-03-11 17:00:03 +0700

    React_Design-Modal-Create-User

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,6 +3,9 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './userManager.scss';
 import { getAllUsers } from '../../services/userService'
+import { bind } from 'lodash';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
 
 
@@ -10,7 +13,8 @@ class UserManage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
@@ -23,11 +27,23 @@ class UserManage extends Component {
         }
         console.log('get all user from nodeJS: ', response)
     }
+
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
     /**
      * Life Circle
      * Run component
      * 1. Run construct -> init state
-     * 2. DidMount (set state)
+     * 2. DidMount (set state): born >< unmount
      * 3. Render
      */
 
@@ -36,7 +52,16 @@ class UserManage extends Component {
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test={'abc'} />
                 <div className='title text-center'>Manage User</div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3'
+                        onClick={() => this.handleAddNewUser()}
+                    ><i className="fas fa-plus"></i>Add new User</button>
+                </div>
                 <div className='user-table mt-3 mx-1'>
                     <table id="customers">
                         <tr>
@@ -48,7 +73,6 @@ class UserManage extends Component {
                         </tr>
                         {
                             arrUsers && arrUsers.map((item, index) => {
-                                console.log('check: ', item, index)
                                 return (
                                     <tr>
                                         <td>{item.email}</td>
