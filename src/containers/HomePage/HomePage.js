9230e892d9f5ace0db865e7ea81104feacf7fb82
Ahryxx <phvanthanh06@gmail.com>
2025-03-22 15:23:52 +0700

    React_50

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty.js';
+import MedicalFacility from './Section/MedicalFacility.js';
+import OutStandingDoctor from './Section/OutStandingDoctor.js';
+import './HomePage.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import HandBook from './Section/HandBook.js';
 class HomePage extends Component {
 
     render() {
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+        };
         return (
             <div>
                 <HomeHeader />
-                <Specialty />
-                <div style={{ height: '300px' }}></div>
+                <Specialty
+                    settings={settings} />
+                <MedicalFacility
+                    settings={settings} />
+                <OutStandingDoctor
+                    settings={settings} />
+                <HandBook
+                    settings={settings} />
             </div>
         );
     }
