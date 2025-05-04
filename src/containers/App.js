e29213c5cee75a0cbdb335e103ab10a1e7e625ad
Ahryxx <phvanthanh06@gmail.com>
2025-05-04 11:12:03 +0700

    Menu-Phan-Quyen-pt.1

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,6 +16,7 @@ import { CustomToastCloseButton } from "../components/CustomToast";
 import HomePage from "./HomePage/HomePage.js";
 import CustomScrollbars from "../components/CustomScrollbars.js";
 import DetailDoctor from "./Patient/Doctor/detailDoctor.js";
+import Doctor from "../routes/Doctor.js";
 
 class App extends Component {
   handlePersistorState = () => {
@@ -52,6 +53,10 @@ class App extends Component {
                   <Route
                     path={path.SYSTEM}
                     component={userIsAuthenticated(System)}
+                  />
+                  <Route
+                    path={"/doctor/"}
+                    component={userIsAuthenticated(Doctor)}
                   />
                   <Route path={path.HOMEPAGE} component={HomePage} />
                   <Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,55 +1,76 @@
 export const adminMenu = [
-    { //Quản lí người dùng
-        name: 'menu.admin.manage-user',
-        menus: [
-            {
-                name: 'menu.admin.crud',
-                link: '/system/user-manage'
-            },
+  {
+    //Quản lí người dùng
+    name: "menu.admin.manage-user",
+    menus: [
+      {
+        name: "menu.admin.crud",
+        link: "/system/user-manage",
+      },
 
-            {
-                name: 'menu.admin.crud-redux',
-                link: '/system/user-redux'
-            },
+      {
+        name: "menu.admin.crud-redux",
+        link: "/system/user-redux",
+      },
 
-            {
-                name: 'menu.admin.manage-doctor',
-                link: '/system/manage-doctor'
-            },
+      {
+        name: "menu.admin.manage-doctor",
+        link: "/system/manage-doctor",
+      },
 
-            {
-                name: 'menu.admin.manage-admin',
-                link: '/system/manage-admin'
-            }
-        ]
-    },
-    { //Quản lí phòng khám
-        name: 'menu.admin.clinic',
-        menus: [
-            {
-                name: 'menu.admin.manage-clinic',
-                link: '/system/manage-clinic'
-            },
+      //   {
+      //     name: "menu.admin.manage-admin",
+      //     link: "/system/manage-admin",
+      //   },
+      {
+        //Quản lí người dùng
 
-        ]
-    },
-    { //Quản lí chuyên khoa
-        name: 'menu.admin.specialty',
-        menus: [
-            {
-                name: 'menu.admin.manage-specialty',
-                link: '/system/manage-specialty'
-            },
+        name: "menu.doctor.manage-schedule",
+        link: "/doctor/manage-schedule",
+      },
+    ],
+  },
+  {
+    //Quản lí phòng khám
+    name: "menu.admin.clinic",
+    menus: [
+      {
+        name: "menu.admin.manage-clinic",
+        link: "/system/manage-clinic",
+      },
+    ],
+  },
+  {
+    //Quản lí chuyên khoa
+    name: "menu.admin.specialty",
+    menus: [
+      {
+        name: "menu.admin.manage-specialty",
+        link: "/system/manage-specialty",
+      },
+    ],
+  },
+  {
+    //Quản lí Cẩm nang
+    name: "menu.admin.handbook",
+    menus: [
+      {
+        name: "menu.admin.manage-handbook",
+        link: "/system/manage-handbook",
+      },
+    ],
+  },
+];
 
-        ]
-    },
-    { //Quản lí Cẩm nang
-        name: 'menu.admin.handbook',
-        menus: [
-            {
-                name: 'menu.admin.manage-handbook',
-                link: '/system/manage-handbook'
-            },
-        ]
-    },
+export const doctorMenu = [
+  {
+    name: "menu.admin.manage-user",
+    menus: [
+      {
+        //Quản lí người dùng
+        name: "menu.doctor.manage-schedule",
+        link: "/doctor/manage-schedule",
+      },
+    ],
+  },
 ];
